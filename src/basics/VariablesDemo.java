a9cde857e5b5ea4b33234eb62a6cdb5970e1871a
lokanath99 <lokanath1912@gmail.com>
2022-10-11 21:52:38 +0530

    variable excercise with unary and arthematic operators

---
+++ src/basics/VariablesDemo.java
@@ -0,0 +1,46 @@
+package basics;
+
+public class VariablesDemo {
+	
+	/*
+	 * int float double char 
+	 * 
+	 * char noun[] = {'p','e','n'}
+	 * 
+	 *                 
+	 *               0   1   2
+	 *string 
+	 *Boolean 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// datatype varableName = value
+//		String noun = "pen";//noun = pen\n
+//		char charr = '#'; 
+//		System.out.println(charr);
+//		/*
+//		 * 
+//		 * 
+//		 * 
+//		 */
+//		charr = 'a';
+//		System.out.println(charr);
+//		Boolean t = true;
+//		Boolean f = false;
+//		
+//		System.out.println(f);
+		
+//		operators
+
+		int a = 10;
+		int b = 6;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println((double)a/b);//Q 1.666666667
+		System.out.println(a%b);//R 4
+	}
+
+}
